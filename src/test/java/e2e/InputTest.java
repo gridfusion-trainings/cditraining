18b9c70994ba6e8bc9bd2f478fcb88dae4b677dd
Michael PalotasVM <michael.palotas@gridfusion.net>
2014-03-19 22:44:56 +0100

    initial checkin

--- src/test/java/e2e/InputTest.java
+++ src/test/java/e2e/InputTest.java
@@ -1,16 +1,23 @@
 package e2e;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class InputTest {
 	
 	@Test
-	public void fillPage() throws InterruptedException{
+	public void fillPage() throws InterruptedException, MalformedURLException{
 		
-		WebDriver driver = new FirefoxDriver();
+		DesiredCapabilities capability = DesiredCapabilities.chrome();
+		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
+		
 		driver.get("http://localhost:8080/tmf2");
 		driver.findElement(By.id("firstname")).sendKeys("MICHAEL");
 		driver.findElement(By.id("lastname")).sendKeys("PALOTAS");
