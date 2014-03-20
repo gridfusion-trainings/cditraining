bdb23bd264813a78f6ac608e60223cadccddb785
Michael PalotasVM <michael.palotas@gridfusion.net>
2014-03-20 11:21:12 +0100

    added new files

---
+++ src/test/java/e2e/InputIntegrationTest.java
@@ -0,0 +1,36 @@
+package e2e;
+
+import interfaces.IntegrationTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+//mark class as an integration test
+@Category(IntegrationTest.class)
+public class InputIntegrationTest {
+	
+	@Test
+	public void fillPage() throws InterruptedException, MalformedURLException{
+		
+		DesiredCapabilities capability = DesiredCapabilities.chrome();
+		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
+		
+		driver.get("http://localhost:8080/tmf2");
+		driver.findElement(By.id("firstname")).sendKeys("MICHAEL");
+		driver.findElement(By.id("lastname")).sendKeys("PALOTAS");
+		driver.findElement(By.id("dob")).sendKeys("02/09/1936");
+		driver.findElement(By.id("submitbutton")).click();
+		Thread.sleep(2000);
+		driver.quit();
+	}
+	
+
+}
