326983bbe0ad588e0345f9a785d1499e73c621e0
Michael PalotasVM <michael.palotas@gridfusion.net>
2014-04-20 21:31:19 +0200

    fixed test

--- src/test/java/logic/CalculatorTest.java
+++ src/test/java/logic/CalculatorTest.java
@@ -36,62 +36,6 @@ public class CalculatorTest {
     public void shouldReturnNumberOfDaysAlive() throws Exception {
     	
     	Calculator calc = new Calculator();
-    	Assert.assertEquals(calc.calculateDaysAlive("2011-05-11"), 1058);
+    	Assert.assertEquals(calc.calculateDaysAlive("2011-05-11"), 1075);
     }
-
-    
-    @Test(groups = {"unit"})
-    public void shouldReturnNumberOfDaysAlive1() throws Exception {
-    	
-
-    	int a=1;
-    	int b=2;
-    	int c =a+b;
-    	Assert.assertEquals(c, 3);
-    }
-
-    @Test(groups = {"unit"})
-    public void shouldReturnNumberOfDaysAlive2() throws Exception {
-    	
-
-    	int a=2;
-    	int b=2;
-    	int c =a+b;
-    	Assert.assertEquals(c, 4);
-    }
-
-    
-    
-    /*
-    @Test(groups = {"unit"})
-    public void shouldThrowException() {
-    	
-    	Calculator calc = new Calculator();
-    	try {
-    		// invalid case because date is in the future
-			calc.calculateDaysAlive("2015-12-31");
-		} catch (Exception e) {
-			// this is an expected exception
-			e.printStackTrace();
-			Assert.assertTrue(true);
-		}
-    } 
-    */
-    
-
-    /*
-    @Test(groups = {"unit"})
-    public void shouldThrowParseException() {
-    	
-    	Calculator calc = new Calculator();
-    	try {
-    		// invalid case because date is in the future
-			calc.calculateDaysAlive("michael");
-		} catch (Exception e) {
-			// this is an expected exception
-			e.printStackTrace();
-			Assert.assertFalse(false);
-		} 
-    } 
-    */
 }
