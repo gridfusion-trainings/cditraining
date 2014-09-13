04281157aaca1a1c0501a8f6bdfaa8b7a9e56558
Michael PalotasVM <michael.palotas@gridfusion.net>
2014-09-13 20:55:09 +0200

    added test

--- src/test/java/logic/CalculatorTest.java
+++ src/test/java/logic/CalculatorTest.java
@@ -13,6 +13,10 @@ import org.testng.annotations.Test;
  */
 public class CalculatorTest {
 	
+	@Test(groups = {"unit"})
+	public void anotherTest(){
+		Assert.assertEquals(true, true);
+	}
 
     @Test(groups = {"unit"})
     public void shouldReturnFullnameTest() throws InputIsEmptyException {
