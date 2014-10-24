668b0653176df2aeefef1cf692441a547caad62a
Michael PalotasVM <michael.palotas@gridfusion.net>
2014-10-24 06:51:07 +0200

    last commits before branching off to LGT CDI course

--- src/test/java/e2e/EndToEndTest.java
+++ src/test/java/e2e/EndToEndTest.java
@@ -48,10 +48,10 @@ public class EndToEndTest {
 
 	    
 	    return new Object[][]{
-	    	//{DesiredCapabilities.firefox()},
-	    	//{DesiredCapabilities.chrome()},
+	    	{DesiredCapabilities.firefox()},
+	    	{DesiredCapabilities.chrome()},
 	    	//{DesiredCapabilities.safari()},	        
-	    	{androidWeb},
+	    	//{androidWeb},
 	    	//{ipad},
 	    };
 	  }
