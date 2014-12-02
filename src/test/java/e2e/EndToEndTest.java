63d2d0ea6655a1d0de8f68862dec444fb58b7ef2
Michael PalotasVM <michael.palotas@gridfusion.net>
2014-12-02 16:03:05 +0100

    changed test

--- src/test/java/e2e/EndToEndTest.java
+++ src/test/java/e2e/EndToEndTest.java
@@ -86,7 +86,7 @@ public class EndToEndTest {
 			driver.findElement(By.id("firstname")).sendKeys("Michael");
 			driver.findElement(By.id("lastname")).sendKeys("Palotas");			
 			driver.findElement(By.id("submitbutton")).click();	
-			Assert.assertEquals(driver.getTitle(), "MF Engineering");
+			Assert.assertEquals(driver.getTitle(), "LGT Training Result");
 		}
 		finally {
 			Thread.sleep(5000);
