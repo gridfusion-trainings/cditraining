f26c5ff625802360a24b11d4ca8b63be6f126d49
e34 <michael.palotas@element34.net>
2016-10-21 14:39:06 +0200

    changed startup script and PROD URL in e2e test

--- start.sh
+++ start.sh
@@ -10,13 +10,13 @@ sudo docker rm --force `sudo docker ps -qa`
 
 
 echo "starting jenkins"
-sudo docker run -d -p 8080:8080 -p 50000:50000 -v /home/e34/jenkins_home:/var/jenkins_home palotas/jenkins-cdi:0.1 
+sudo docker run -d --name jenkins -p 8080:8080 -p 50000:50000 -v /home/e34/jenkins_home:/var/jenkins_home palotas/jenkins-cdi:0.1
 
 echo "starting tomcat QA"
-sudo docker run -d -p 9999:8080 --expose=8080 palotas/tomcat7-cdi 
+sudo docker run -d --name tomcat-QA -p 9999:8080 --expose=8080 palotas/tomcat7-cdi
 
 echo "starting tomcat PRODUCTION"
-sudo docker run -d -p 9998:8080 --expose=8080 palotas/tomcat7-cdi
+sudo docker run -d --name tomcat-PROD -p 9998:8080 --expose=8080 palotas/tomcat7-cdi
 
 echo "starting sonarqube"
 sudo docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube
