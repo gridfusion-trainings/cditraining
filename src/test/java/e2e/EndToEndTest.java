b56472fd7e39c8de660b50a35d46a185e3b0acd4
e34 <michael.palotas@element34.net>
2017-05-23 15:29:34 +0200

    adding SBOX URL 

--- src/test/java/e2e/EndToEndTest.java
+++ src/test/java/e2e/EndToEndTest.java
@@ -19,7 +19,8 @@ import org.testng.annotations.Test;
 public class EndToEndTest {
 	
 	//local remote webdriver
-	public final String GRIDURL="http://10.0.2.15:4444/wd/hub";
+	//public final String GRIDURL="http://10.0.2.15:4444/wd/hub";
+	public final String GRIDURL="https://789b1ea7eca7.element34.net/wd/hub";
 
 	@DataProvider(name = "platforms", parallel = true)
 	  public Object[][] getCapabilities() {
