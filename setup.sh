ba27f066f4024e03f521b9ca4c51ddf8d1cf5f99
Michael Palotas <michael.palotas@element34.net>
2017-06-05 12:37:48 +0200

    replacing filename with $file

--- setup.sh
+++ setup.sh
@@ -4,14 +4,14 @@ file="/home/e34/Downloads/jobs.tar.gz"
 if [ -f "$file" ]
 then
 	echo "Old $file found. Deleting it"
-	rm -rf /home/e34/Downloads/jobs.tar.gz
+	rm -rf $file
 fi
 
 #wget -P /home/e34/Downloads https://s3.amazonaws.com/e34/jobs.tar.gz	
 wget -P /home/e34/Downloads http://192.168.1.103:80/jobs.tar.gz
-sudo chmod 777 /home/e34/Downloads/jobs.tar.gz
+sudo chmod 777 $file
 echo "untar jobs.tar.gz into jenkins_home"
-tar -xvf /home/e34/Downloads/jobs.tar.gz -C /home/e34/jenkins_home/
+tar -xvf $file -C /home/e34/jenkins_home/
 
 
 
