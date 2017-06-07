7ef883b44c8a44cdb989e746d22e1582769a2de2
e34 <michael.palotas@element34.net>
2017-06-07 11:12:51 +0200

    switched download to S3

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@ then
 	rm -rf $file
 fi
 
-#wget -P /home/e34/Downloads https://s3.amazonaws.com/e34/jobs.tar.gz	
-wget -P /home/e34/Downloads http://192.168.1.103:80/jobs.tar.gz
+wget -P /home/e34/Downloads https://s3.amazonaws.com/e34/jobs.tar.gz
+#wget -P /home/e34/Downloads http://192.168.1.103:80/jobs.tar.gz
 sudo chmod 777 $file
 echo "untar jobs.tar.gz into jenkins_home"
 tar -xvf $file -C /home/e34/jenkins_home/
