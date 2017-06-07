8ff4df45357cbaf2bff816aa8c3d3009ff2c4fd1
e34 <michael.palotas@element34.net>
2017-06-07 10:41:53 +0200

    added --rm to docker container startup

--- start.sh
+++ start.sh
@@ -10,16 +10,16 @@ sudo docker rm --force `sudo docker ps -qa`
 
 
 #echo "starting jenkins"
-sudo docker run -d --name jenkins -p 8080:8080 -p 50000:50000 -v /home/e34/jenkins_home/jobs:/var/jenkins_home/jobs palotas/e34_jenkins_noadmin:0.7
+sudo docker run --rm -d --name jenkins -p 8080:8080 -p 50000:50000 -v /home/e34/jenkins_home/jobs:/var/jenkins_home/jobs palotas/e34_jenkins_noadmin:0.7
 
 echo "starting tomcat QA"
-sudo docker run -d --name tomcat-QA -p 9999:8080 --expose=8080 palotas/tomcat7-cdi
+sudo docker run --rm -d --name tomcat-QA -p 9999:8080 --expose=8080 palotas/tomcat7-cdi
 
 echo "starting tomcat PRODUCTION"
-sudo docker run -d --name tomcat-PROD -p 9998:8080 --expose=8080 palotas/tomcat7-cdi
+sudo docker run --rm -d --name tomcat-PROD -p 9998:8080 --expose=8080 palotas/tomcat7-cdi
 
 echo "starting sonarqube"
-sudo docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube
+sudo docker run --rm -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube
 
 echo "setting git timeout to 5 minutes"
 cd /home/e34/workspace/cditraining
