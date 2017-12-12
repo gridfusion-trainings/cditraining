df046b4f23ec8678a228d031802e29b7b713ad66
e34 <you@example.com>
2017-12-12 11:30:58 +0100

    update endpoint

--- src/test/java/e2e/EndToEndTest.java
+++ src/test/java/e2e/EndToEndTest.java
@@ -34,10 +34,10 @@ public class EndToEndTest {
 	public static String URLFactory(String environment) {
 		
 		Map<String, String> urls = new HashMap<String, String>();
-		urls.put("QA", "http://172.17.0.3:8080/vpbank/"); //goes to docker container
-		urls.put("AWS", "http://ec2-18-195-78-49.eu-central-1.compute.amazonaws.com:9998/vpbank");
-		urls.put("PRODUCTION", "http://172.17.0.4:8080/vpbank");
-		urls.put("VAGRANT", "http://192.168.1.150:8080/vpbank/");
+		urls.put("QA", "http://172.17.0.3:8080/digi/"); //goes to docker container
+		urls.put("AWS", "http://ec2-18-195-78-49.eu-central-1.compute.amazonaws.com:9998/digi");
+		urls.put("PRODUCTION", "http://172.17.0.4:8080/digi");
+		urls.put("VAGRANT", "http://192.168.1.150:8080/digi/");
 		
 		String url = (String) urls.get(environment);
 		return url;
