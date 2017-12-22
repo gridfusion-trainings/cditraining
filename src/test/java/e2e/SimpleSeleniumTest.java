e1b44f49d2573719c24c1dcb98455042886a8018
e34 <michael.palotas@element34.com>
2017-12-22 12:37:55 +0100

    clean up test

--- src/test/java/e2e/SimpleSeleniumTest.java
+++ src/test/java/e2e/SimpleSeleniumTest.java
@@ -7,6 +7,7 @@ import java.net.URL;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -17,55 +18,16 @@ import org.testng.annotations.Test;
 
 public class SimpleSeleniumTest {
 
-	@Test
-	public void ciscoTest2() throws IOException, InterruptedException {
+    @Test
+    public void simpleTest() throws InterruptedException, MalformedURLException {
+        DesiredCapabilities caps = DesiredCapabilities.chrome();
 
+        RemoteWebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), caps);
+        driver.get("https://google.com");
+        Thread.sleep(3000);
 
-		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-		capabilities.setCapability("video", true);
-		RemoteWebDriver driver =
-				new RemoteWebDriver(new URL("http://localhost:4444"), capabilities);
+        driver.quit();
 
-
-
-		driver.manage().window().maximize();
-		WebDriverWait wait = new WebDriverWait(driver, 10);
-
-
-		//System.out.println(getVideoURLNew(driver));
-
-		//go to homepage
-		Reporter.log("Opening CS homepage");
-		driver.get("https://www.credit-suisse.com/ch/en.html");
-		//System.out.println("http://"+HUB_HOST+":4444/grid/resources/liveView?session=" + driver.getSessionId().toString());
-
-		//click on login
-		Reporter.log("Clicking login button");
-		driver.findElement(By.id("btn_login")).click();
-		Thread.sleep(3000);
-
-		Reporter.log("Clicking Online Banking Link ");
-		Thread.sleep(5000);
-		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Online Banking")));
-		driver.findElement(By.xpath("//*[@id=\"mod_login_selector\"]/div/section/div[1]/section[1]/ul/li[1]/a")).click();
-
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("here.")));
-		driver.findElement(By.linkText("here.")).click();
-
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("username")));
-		driver.findElement(By.id("username")).sendKeys("4711");
-
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("button")));
-		driver.findElement(By.className("button")).click();
-
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("error")));
-		Assert.assertTrue(driver.findElement(By.className("error")).getText().contains("Your login is invalid"));
-
-		//Thread.sleep(3000);
-
-		driver.quit();
-	}
-
-
+    }
 
 }
