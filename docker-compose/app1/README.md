7ae0d5159daef8206e0151b24ac0366b0019c256
Michael Palotas <michael.palotas@element34.com>
2018-10-29 21:07:34 +0100

    cleanup

---
+++ docker-compose/app1/README.md
@@ -0,0 +1,6 @@
+1. Build docker image: ´´´docker build -t palotas/webapp -f Dockerfile1```
+
+2. Run app as regular docker application: ```docker run --rm -p 5000:5000 palotas/webapp```
+
+3. Run with docker-compose: ```docker-compose -f docker-compose1.yml up```
+
--- docker-compose/app1/app1.py
+++ docker-compose/app1/app1.py
@@ -4,18 +4,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
-
-
 @app.route('/')
 def hello():
     return 'Hello from E34!'
