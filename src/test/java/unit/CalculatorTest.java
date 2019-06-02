408a497ed61aa5579a0059a835c9a956da3db693
Michael Palotas <michael.palotas@element34.com>
2019-06-02 20:18:23 +0200

    make build unstable

--- src/test/java/unit/CalculatorTest.java
+++ src/test/java/unit/CalculatorTest.java
@@ -37,59 +37,59 @@ public class CalculatorTest {
 
 	}
 	
-
-	@Test(groups="unit")
-	public void shouldRemoveMultipleWhitespaceInName() {
-
-		InputProcessor mockedInputProcessor = mock(InputProcessor.class);
-
-		// stubbing appears before the actual execution
-		when(mockedInputProcessor.stripWhitespaces("Mi ch ael    Palo tas")).thenReturn("MichaelPalotas");
-
-
-		Calculator calc = new Calculator();
-		calc.setInputProcessor(mockedInputProcessor);
-
-		Assert.assertEquals(calc.calculateNumberOfChars("Mi ch ael    Palo tas"), 14);
-
-	}
-
-
-    @Test(groups = {"unit"})
-    public void shouldReturnFullname() throws InputIsEmptyException {
-
-        Calculator calc = new Calculator();
-        Assert.assertEquals(calc.calculateName("Michael", "Palotas"), "Michael Palotas");
-    }
-
-
-
-    @Test(groups = {"unit"}, expectedExceptions=InputIsEmptyException.class)
-    public void shouldThrowInputIsEmptyExceptionBothNames() throws InputIsEmptyException {
-
-        Calculator calc = new Calculator();
-        Assert.assertEquals("Michael Palotas", calc.calculateName("", ""));
-    }
-
-    @Test(groups = {"unit"}, expectedExceptions=InputIsEmptyException.class)
-    public void shouldThrowInputIsEmptyExceptionFirstName() throws InputIsEmptyException {
-
-        Calculator calc = new Calculator();
-        Assert.assertEquals("Michael Palotas", calc.calculateName("", "Palotas"));
-    }
-
-    @Test(groups = {"unit"}, expectedExceptions=InputIsEmptyException.class)
-    public void shouldThrowInputIsEmptyExceptionLastName() throws InputIsEmptyException {
-
-        Calculator calc = new Calculator();
-        Assert.assertEquals("Michael Palotas", calc.calculateName("Michael", ""));
-    }
-
-    @Test(groups = {"unit"})
-    public void failedTest() throws InputIsEmptyException {
-
-        Assert.assertTrue(true);
-    }
+//
+//	@Test(groups="unit")
+//	public void shouldRemoveMultipleWhitespaceInName() {
+//
+//		InputProcessor mockedInputProcessor = mock(InputProcessor.class);
+//
+//		// stubbing appears before the actual execution
+//		when(mockedInputProcessor.stripWhitespaces("Mi ch ael    Palo tas")).thenReturn("MichaelPalotas");
+//
+//
+//		Calculator calc = new Calculator();
+//		calc.setInputProcessor(mockedInputProcessor);
+//
+//		Assert.assertEquals(calc.calculateNumberOfChars("Mi ch ael    Palo tas"), 14);
+//
+//	}
+//
+//
+//    @Test(groups = {"unit"})
+//    public void shouldReturnFullname() throws InputIsEmptyException {
+//
+//        Calculator calc = new Calculator();
+//        Assert.assertEquals(calc.calculateName("Michael", "Palotas"), "Michael Palotas");
+//    }
+//
+//
+//
+//    @Test(groups = {"unit"}, expectedExceptions=InputIsEmptyException.class)
+//    public void shouldThrowInputIsEmptyExceptionBothNames() throws InputIsEmptyException {
+//
+//        Calculator calc = new Calculator();
+//        Assert.assertEquals("Michael Palotas", calc.calculateName("", ""));
+//    }
+//
+//    @Test(groups = {"unit"}, expectedExceptions=InputIsEmptyException.class)
+//    public void shouldThrowInputIsEmptyExceptionFirstName() throws InputIsEmptyException {
+//
+//        Calculator calc = new Calculator();
+//        Assert.assertEquals("Michael Palotas", calc.calculateName("", "Palotas"));
+//    }
+//
+//    @Test(groups = {"unit"}, expectedExceptions=InputIsEmptyException.class)
+//    public void shouldThrowInputIsEmptyExceptionLastName() throws InputIsEmptyException {
+//
+//        Calculator calc = new Calculator();
+//        Assert.assertEquals("Michael Palotas", calc.calculateName("Michael", ""));
+//    }
+//
+//    @Test(groups = {"unit"})
+//    public void failedTest() throws InputIsEmptyException {
+//
+//        Assert.assertTrue(true);
+//    }
 }
 
 
